e4e9f4a9865c36d611a5cf2dcec193e38a711033
Nikolas Graham <Lichtschwert@live.com>
2019-10-26 17:25:18 -0700

    Added more logic and tests

---
+++ Configuration.Tests/EnvironmentConfigTests.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Configuration;
+
+namespace Configuration.Tests
+{
+    [TestClass]
+    public class EnvironmentConfigTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void EnvironmentConfig_SetEnvironmentVariable_NullVariables_ReturnsFalse()
+        {
+            IConfig testConfig = new EnvironmentConfig();
+
+            bool works = testConfig.SetConfigValue(null, null);
+
+            Assert.IsFalse(works);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void EnvironmentConfig_SetEnvironmentVariable_NullName_ReturnsFalse()
+        {
+            IConfig testConfig = new EnvironmentConfig();
+
+            bool works = testConfig.SetConfigValue(null, "testValue");
+
+            Assert.IsFalse(works);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void EnvironmentConfig_SetEnvironmentVariable_NullValue_ReturnsFalse()
+        {
+            IConfig testConfig = new EnvironmentConfig();
+
+            bool works = testConfig.SetConfigValue("testName", null);
+
+            Assert.IsFalse(works);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void EnvironmentConfig_SetEnvironmentVariable_NameWithSpaces_ReturnsFalse()
+        {
+            IConfig testConfig = new EnvironmentConfig();
+
+            bool works = testConfig.SetConfigValue(" ", "testValue");
+
+            Assert.IsFalse(works);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void EnvironmentConfig_SetEnvironmentVariable_ValueWithSpaces_ReturnsFalse()
+        {
+            IConfig testConfig = new EnvironmentConfig();
+
+            bool works = testConfig.SetConfigValue("testName", " ");
+
+            Assert.IsFalse(works);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void EnvironmentConfig_SetEnvironmentVariable_NameEmptyString_ReturnsFalse()
+        {
+            IConfig testConfig = new EnvironmentConfig();
+
+            bool works = testConfig.SetConfigValue("", "testValue");
+
+            Assert.IsFalse(works);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void EnvironmentConfig_SetEnvironmentVariable_ValueEmptyString_ReturnsFalse()
+        {
+            IConfig testConfig = new EnvironmentConfig();
+
+            bool works = testConfig.SetConfigValue("testName", "");
+
+            Assert.IsFalse(works);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void EnvironmentConfig_SetEnvironmentVariable_NameWithEqualsSign_ReturnsFalse()
+        {
+            IConfig testConfig = new EnvironmentConfig();
+
+            bool works = testConfig.SetConfigValue("99=99=", "testValue");
+
+            Assert.IsFalse(works);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void EnvironmentConfig_SetEnvironmentVariable_ValueWithEqualsSign_ReturnsFalse()
+        {
+            IConfig testConfig = new EnvironmentConfig();
+
+            bool works = testConfig.SetConfigValue("testName", "83=234=234=");
+
+            Assert.IsFalse(works);
+        }
+
+        [TestMethod]
+        public void EnvironmentConfig_SetEnvironmentVariable_NonNullVariables_ReturnsTrue()
+        {
+            IConfig testConfig = new EnvironmentConfig();
+
+            bool works = testConfig.SetConfigValue("testName", "testValue");
+
+            Assert.IsTrue(works);
+        }
+    }
+}
---
+++ Configuration/EnvironmentConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Configuration
+{
+    public class EnvironmentConfig : IConfig
+    {
+        public bool GetConfigValue(string name, string? value)
+        {
+            if(name is null) { throw new ArgumentNullException(nameof(name)); }
+            if (name == " " || name.Length == 0) { throw new ArgumentException("No name given!"); }
+
+            try
+            {
+                value = Environment.GetEnvironmentVariable(name);
+                return true;
+            }
+            catch(ArgumentNullException)
+            {
+                return false;
+            }
+        }
+
+        public bool SetConfigValue(string name, string? value)
+        {
+            if(name is null) { throw new ArgumentNullException(nameof(name)); }
+            if(value is null) { throw new ArgumentNullException(nameof(value)); }
+
+            if(name.Contains(" ") || name.Length == 0) { throw new ArgumentException("No name given!"); }
+            if (name.Contains("=")) { throw new ArgumentException("Cannot have name wih equals!"); }
+            if (value.Contains(" ") || value.Length == 0) { throw new ArgumentException("No value given!"); }
+            if (value.Contains("=")) { throw new ArgumentException("Cannot have value wih equals!"); }
+
+            try
+            {
+                Environment.SetEnvironmentVariable(name, value);
+                return true;
+            }
+            catch(ArgumentException)
+            {
+                return false;
+            }
+        }
+    }
+}
