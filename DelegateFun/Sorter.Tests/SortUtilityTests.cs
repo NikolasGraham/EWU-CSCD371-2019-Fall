a68125fc5bb006e77f71fe2b495a804b4ad915b3
Nikolas Graham <Lichtschwert@live.com>
2019-11-16 16:15:25 -0800

    Assignment7

--- DelegateFun/Sorter.Tests/SortUtilityTests.cs
+++ DelegateFun/Sorter.Tests/SortUtilityTests.cs
@@ -1,13 +1,64 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace Sorter.Tests
 {
     [TestClass]
     public class SortUtilityTests
     {
+
         [TestMethod]
-        public void SortUtility_ShouldSortAscending_UsingAnAnonymousMethod()
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SortUtility_NullArray_ThrowsException()
         {
+            SortUtility sortingUtils = new SortUtility();
+
+            sortingUtils.selectionSort(null, (int left, int right) => left > right);
+        }
+
+        [TestMethod]
+        [DataRow(new int[] {1,3,5,7,9,2,4,6,8}, new int[] {1,2,3,4,5,6,7,8,9})]
+        [DataRow(new int[] {1,9,2,8,3,7,4,6,5}, new int[] {1,2,3,4,5,6,7,8,9})]
+        public void SortUtility_ShouldSortAscending_UsingAnAnonymousMethod(int[] toSort, int[] expected)
+        {
+            SortUtility sortingUtils = new SortUtility();
+            intComparer comparer = delegate (int left, int right)
+            {
+                return left < right;
+            };
+
+            sortingUtils.selectionSort(toSort, comparer);
+
+            CollectionAssert.AreEqual(expected, toSort);
+        }
+
+        [TestMethod]
+        [DataRow(new int[] {1,63,632,727,247,472}, new int[] {727,632,472,247,63,1})]
+        [DataRow(new int[] {1,2,3,4,5,6,7,8,9}, new int[] {9,8,7,6,5,4,3,2,1})]
+        public void SortUtility_ShouldSortDescending_UsingLambdaExpression(int[] toSort, int[] expected)
+        {
+            SortUtility sortingUtils = new SortUtility();
+
+            sortingUtils.selectionSort(toSort, (int left, int right) => left > right);
+
+            CollectionAssert.AreEqual(expected, toSort);
+        }
+
+        [TestMethod]
+        [DataRow(new int[] {1,2,3,4,5,6,7,8,9}, new int[] {4,3,2,1,5,6,7,8,9})]
+        [DataRow(new int[] {1,9,2,8,3,7,4,6,5}, new int[] {4,3,2,1,5,6,7,8,9})]
+        [DataRow(new int[] {9,8,7,6,5}, new int[] {5,6,7,8,9})]
+        [DataRow(new int[] {1,2,3,4}, new int[] {4,3,2,1})]
+        public void SortUtility_ShouldSortDescendingUnderFiveThenAscendToMax_UsingLambdaStatement(int[] toSort, int[] expected)
+        {
+            SortUtility sortingUtils = new SortUtility();
+
+            sortingUtils.selectionSort(toSort, (int left, int right) => {
+                if(left > 4 || right > 4) { return left < right; }
+                return left > right;
+            });
+
+            CollectionAssert.AreEqual(expected, toSort);
         }
     }
 }
--- DelegateFun/Sorter/SortUtility.cs
+++ DelegateFun/Sorter/SortUtility.cs
@@ -2,9 +2,32 @@
 
 namespace Sorter
 {
+    public delegate bool intComparer(int left, int right);
     public class SortUtility
     {
         // Sort method should be implemented here
         // It should accept an int[] and a delegate you define that performs the actual comparison
+        public void selectionSort(int[] ara, intComparer compare)
+        {
+            if(ara == null)
+            {
+                throw new ArgumentNullException(nameof(ara));
+            }
+
+            for(int i=0; i < ara.Length-1; i++)
+            {
+                int min = i;
+                for(int k=i+1; k < ara.Length; k++)
+                {
+                    if(compare(ara[k], ara[min]))
+                    {
+                        min = k;
+                    }
+                }
+                int temp = ara[min];
+                ara[min] = ara[i];
+                ara[i] = temp;
+            }
+        }
     }
 }
