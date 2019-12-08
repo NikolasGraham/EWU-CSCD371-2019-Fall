77e59b89d81153e0a837ee03c17f2ea6e842a949
Nikolas Graham <Lichtschwert@live.com>
2019-12-07 21:29:39 -0800

    Added Sort

--- ShoppingList/ShoppingList/Item.cs
+++ ShoppingList/ShoppingList/Item.cs
@@ -1,12 +1,28 @@
-﻿namespace ShoppingList
+﻿using System;
+
+namespace ShoppingList
 {
-    public class Item
+    public class Item : IComparable
     {
         public string Name { get; set; }
 
         public Item(string name)
         {
             Name = name;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(obj is Item)
+            {
+                return ItemCompareTo((Item)obj);
+            }
+            return -1;
+        }
+
+        public int ItemCompareTo(Item item)
+        {
+            return Name.CompareTo(item.Name);
         }
     }
 }
--- ShoppingList/ShoppingList/MainWindowViewModel.cs
+++ ShoppingList/ShoppingList/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 
@@ -22,9 +23,10 @@ namespace ShoppingList
         // Commands
         public ICommand AddItemCommand { get; }
         public ICommand DeleteItemCommand { get; }
+        public ICommand SortListCommand { get; }
 
         // Items
-        public ObservableCollection<Item> Items { get; } = new ObservableCollection<Item>();
+        public ObservableCollection<Item> Items { get; private set; } = new ObservableCollection<Item>();
 
         private string _ItemToAdd = "";
         public string ItemToAdd
@@ -44,6 +46,7 @@ namespace ShoppingList
         {
             AddItemCommand = new Command(OnAddItem);
             DeleteItemCommand = new Command(OnDeleteItem);
+            SortListCommand = new Command(OnSortList);
 
             Items.Add(new Item("Apples"));
             Items.Add(new Item("Bananas"));
@@ -51,13 +54,23 @@ namespace ShoppingList
 
         private void OnAddItem()
         {
-            Items.Add(new Item(ItemToAdd));
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Items)));
+            if(ItemToAdd != "")
+            {
+                Items.Add(new Item(ItemToAdd));
+                ItemToAdd = "";
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Items)));
+            }
         }
 
         private void OnDeleteItem()
         {
             Items.Remove(SelectedItem);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Items)));
+        }
+
+        private void OnSortList()
+        {
+            Items = new ObservableCollection<Item>(Items.OrderBy(i => i));
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Items)));
         }
     }
